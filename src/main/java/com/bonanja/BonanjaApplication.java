17d4b3acba7cbe4b08089d44bcae7859d59ee0ce
msPark09 <parkmisun0909@gmail.com>
2021-05-18 13:49:26 +0900

    Boot 섧정

--- src/main/java/com/bonanja/BonanjaApplication.java
+++ src/main/java/com/bonanja/BonanjaApplication.java
@@ -1,13 +1,47 @@
 package com.bonanja;
 
+import java.util.Collections;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.SessionCookieConfig;
+import javax.servlet.SessionTrackingMode;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
-public class BonanjaApplication {
+public class BonanjaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BonanjaApplication.class, args);
+		
+		log.info("★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★");
+		log.info("★★★★★  RUN BonanjaApplication    ★★★★★★★★★");
+		log.info("★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★");
+
+		SpringApplication.run(KredbSiteApplication.class, args);
+	}
+	
+	@Override
+    	public void onStartup(ServletContext servletContext) throws ServletException {
+
+		super.onStartup(servletContext);
+
+		servletContext
+		    .setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE));  // url호출시  jsessionid 제거
+
+		SessionCookieConfig sessionCookieConfig = servletContext.getSessionCookieConfig();
+		sessionCookieConfig.setHttpOnly(true);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BonanjaApplication.class);
 	}
 
 }
